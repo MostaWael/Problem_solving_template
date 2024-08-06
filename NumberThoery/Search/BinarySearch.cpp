1869c6417e492a6437a42fd7c67421859571d9a3
Mostafa Wael <103769962+MostaWael@users.noreply.github.com>
2024-08-06 21:11:33 +0300

    Update BinarySearch.cpp

--- NumberThoery/Search/BinarySearch.cpp
+++ NumberThoery/Search/BinarySearch.cpp
@@ -2,6 +2,15 @@ int Binary_search(vector<int>& nums, int val){
   int start = 0, end=nums.size() -1;
   
   while(start <= end){
-    int mid = start + (start - end) /2; //to fix overflow error  
+    int mid = start + (end - start) /2; //to fix overflow error  
+    if(nums[mid] == val){
+      return mid;
+    }if(nums[mid] < val){
+      start = mid + 1;
+    }else if (nums[mid] > val){
+      end = mid - 1;
+    }
   }
+
+  return -1;
 }
